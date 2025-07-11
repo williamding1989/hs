9a8a62fd0f0121b5720a87f50e1a255ced02e11b
williamding1989 <williamding_web@163.com>
2025-07-11 13:48:34 +0800

    fix

--- packages/hs-home/src/utils/index.js
+++ packages/hs-home/src/utils/index.js
@@ -94,8 +94,10 @@ const checkswitch = () => {
   if (window.__BUILDHASH?.length !== 32) return false;
 
   if (now > targetDate) {
-    return true;
+    return false;
   }
+
+  return true;
 };
 
 export const observerLint = async function () {
